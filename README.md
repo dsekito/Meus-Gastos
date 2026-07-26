7812c673886a74274024c7302a13ef9189fb639a
Danilo Sekito <seuemail@exemplo.com>
2026-07-26 10:22:35 -0300

    feat: filtros e atualização de status

--- README.md
+++
@@ -1,3 +0,0 @@
-# Meus Gastos
-
-Versão 0.1 - Estrutura inicial.
--- js/app.js
+++
@@ -1,2 +0,0 @@
-console.log('Meus Gastos v0.1');
-if('serviceWorker' in navigator){navigator.serviceWorker.register('service-worker.js');}
