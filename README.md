a93665baab675b978acfcfb68d68398604dec7b3
Danilo Sekito <seuemail@exemplo.com>
2026-07-26 09:33:33 -0300

    Sprint 1 - Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meus Gastos
+
+Versão 0.1 - Estrutura inicial.
---
+++ js/app.js
@@ -0,0 +1,2 @@
+console.log('Meus Gastos v0.1');
+if('serviceWorker' in navigator){navigator.serviceWorker.register('service-worker.js');}
